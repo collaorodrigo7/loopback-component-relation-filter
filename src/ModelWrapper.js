0f68ad923b1c0c85cd038fae35cf2421effef474
Rodrigo Collao <collaorodrigo@ou.edu>
2019-03-22 10:32:19 -0500

    https://github.com/joinbox/loopback-component-relation-filter/pull/6/commits

--- src/ModelWrapper.js
+++ src/ModelWrapper.js
@@ -1,152 +1,160 @@
 module.exports = class ModelWrapper {
 
-    constructor(model, alias = null) {
-        this.model = model;
-        this.connector = this.wrapConnector(model);
-        this.alias = alias;
-    }
+  constructor(model, alias = null) {
+      this.model = model;
+      this.connector = this.wrapConnector(model);
+      this.alias = alias;
+  }
 
-    wrapConnector(model) {
-        return model.dataSource.connector;
-    }
+  wrapConnector(model) {
+      return model.dataSource.connector;
+  }
 
-    getTable() {
-        const schema = this.getSchema();
-        const table = this.getTableName();
-        return (schema) ? `${schema}.${table}`: `${table}`;
-    }
+  getTable() {
+      const schema = this.getSchema();
+      const table = this.getTableName();
+      return (schema) ? `${schema}.${table}`: `${table}`;
+  }
 
-    getTableName() {
-        return this.connector.table(this.getModelName());
-    }
+  getTableName() {
+      return this.connector.table(this.getModelName());
+  }
 
-    getSchema() {
-        return this.connector.schema(this.getModelName());
-    }
+  getSchema() {
+      return this.connector.schema(this.getModelName());
+  }
 
-    getFullyQualifiedTable() {
-        return this.getTable();
-    }
+  getFullyQualifiedTable() {
+      return this.getTable();
+  }
 
-    getAliasedTable() {
-        const alias = this.alias ? ` as ${this.alias}` : '';
-        return `${this.getFullyQualifiedTable()}${alias}`;
-    }
+  getAliasedTable() {
+      const alias = this.alias ? ` as ${this.alias}` : '';
+      return `${this.getFullyQualifiedTable()}${alias}`;
+  }
 
-    getColumnName(key, { alias = this.alias, preserveCase = true } = {}) {
-        const tableName = alias || this.getTable();
-        const columnName = preserveCase ? key : key.toLowerCase();
-        return `${tableName}.${columnName}`;
-    }
+  getColumnName(key, { alias = this.alias, preserveCase = true } = {}) {
+      const tableName = alias || this.getTable();
+      const columnName = preserveCase ? this.getRealColumnName(key) : this.getRealColumnName(key).toLowerCase();
+      return `${tableName}.${columnName}`;
+  }
 
-    getModelName() {
-        return this.model.modelName;
-    }
+  getModelName() {
+      return this.model.modelName;
+  }
 
-    getConnectorName() {
-        return this.connector.name;
-    }
+  getConnectorName() {
+      return this.connector.name;
+  }
 
-    getModelProperties() {
-        return this.model.definition.properties;
-    }
+  getModelProperties() {
+      return this.model.definition.properties;
+  }
 
-    getModelRelations() {
-        return this.model.relations || {};
+  getRealColumnName(key){
+    if(this.model.definition.properties[key][this.connector.name] && this.model.definition.properties[key][this.connector.name].columnName){
+      return this.model.definition.properties[key][this.connector.name].columnName;
+    } else {
+      return key;
     }
+  }
 
-    getRelation(name) {
-        return this.getModelRelations()[name];
-    }
+  getModelRelations() {
+      return this.model.relations || {};
+  }
 
-    /**
-     * Returns the property of the wrapped model that is referenced by the passed relation.
-     *
-     * Example: Book has an m:n relation to Authors stored in book_authors {bookId, authorId}. The
-     * foreign key authorId references the field id of the authors table. To get the name of the
-     * referenced field (id in this case) we need to lookup the definition of the Author model and
-     * get the corresponding key.
-     *
-     * @note: we return null in case we don't find anything so the invoking component (i.e. the
-     * QueryBuilder will be able to make its own assumtions on whitch property to query (e.g. take
-     * the id of the target model)
-     *
-     * @param relation the loopback relation definition
-     * @returns {(string|null)}
-     */
-    getPropertyQueriedThrough(relation) {
-        // ensure that the relation is a mapping that references the wrapped model
-        if (!!relation.modelThrough ||
-            !!relation.modelTo ||
-            relation.modelTo.modelName !== this.model.modelName) {
-            return null;
-        } else {
-            const reverseRelation = Object
-                .values(this.getModelRelations())
-                .find((rel) => {
-                    // check if it is the same through model and if it references the correct entity
-                    return rel.modelThrough.modelName === relation.modelThrough.modelName
-                        && rel.modelTo.modelName === relation.modelFrom.modelName;
-                });
-            // if there is no reverse relation, the relation to this model is unidirectional
-            // and we are not able to determine the referenced field
-            if (reverseRelation) {
-                return reverseRelation.keyFrom;
-            } else {
-                return null;
-            }
-        }
-    }
+  getRelation(name) {
+      return this.getModelRelations()[name];
+  }
 
-    getQueriedRelations(where = {}) {
-        return Object
-            .values(this.getModelRelations())
-            .reduce((queriedRelations, relation) => {
-                if (where[relation.name]) {
-                    queriedRelations.push(relation);
-                }
-                return queriedRelations;
-            }, []);
-    }
+  /**
+   * Returns the property of the wrapped model that is referenced by the passed relation.
+   *
+   * Example: Book has an m:n relation to Authors stored in book_authors {bookId, authorId}. The
+   * foreign key authorId references the field id of the authors table. To get the name of the
+   * referenced field (id in this case) we need to lookup the definition of the Author model and
+   * get the corresponding key.
+   *
+   * @note: we return null in case we don't find anything so the invoking component (i.e. the
+   * QueryBuilder will be able to make its own assumtions on whitch property to query (e.g. take
+   * the id of the target model)
+   *
+   * @param relation the loopback relation definition
+   * @returns {(string|null)}
+   */
+  getPropertyQueriedThrough(relation) {
+      // ensure that the relation is a mapping that references the wrapped model
+      if (!!relation.modelThrough ||
+          !!relation.modelTo ||
+          relation.modelTo.modelName !== this.model.modelName) {
+          return null;
+      } else {
+          const reverseRelation = Object
+              .values(this.getModelRelations())
+              .find((rel) => {
+                  // check if it is the same through model and if it references the correct entity
+                  return rel.modelThrough.modelName === relation.modelThrough.modelName
+                      && rel.modelTo.modelName === relation.modelFrom.modelName;
+              });
+          // if there is no reverse relation, the relation to this model is unidirectional
+          // and we are not able to determine the referenced field
+          if (reverseRelation) {
+              return reverseRelation.keyFrom;
+          } else {
+              return null;
+          }
+      }
+  }
 
-    getIdProperties(options = {}) {
-        // return the ids as an array for backwards compatibility in case of compound ids
-        // (which - in general - is not supported by Loopback)
-        const idName = this.model.getIdName();
-        const ids = [idName];
-        if (options.ignoreAlias === true) {
-            return ids;
-        }
-        return ids.map(id => this.getColumnName(id, options));
-    }
+  getQueriedRelations(where = {}) {
+      return Object
+          .values(this.getModelRelations())
+          .reduce((queriedRelations, relation) => {
+              if (where[relation.name]) {
+                  queriedRelations.push(relation);
+              }
+              return queriedRelations;
+          }, []);
+  }
 
-    isProperty(propertyName) {
-        return Object.prototype.hasOwnProperty.call(this.getModelProperties(), propertyName);
-    }
+  getIdProperties(options = {}) {
+      // return the ids as an array for backwards compatibility in case of compound ids
+      // (which - in general - is not supported by Loopback)
+      const idName = this.model.getIdName();
+      const ids = [idName];
+      if (options.ignoreAlias === true) {
+          return ids;
+      }
+      return ids.map(id => this.getColumnName(id, options));
+  }
 
-    isRelation(propertyName) {
-        return Object.prototype.hasOwnProperty.call(this.getModelRelations(), propertyName);
-    }
+  isProperty(propertyName) {
+      return Object.prototype.hasOwnProperty.call(this.getModelProperties(), propertyName);
+  }
 
-    getQueriedProperties(query = {}, options={}) {
-        return Object
-            .keys(query)
-            .filter(propertyName => this.isProperty(propertyName))
-            .map(propertyName => ({
-                key: this.getColumnName(propertyName, options),
-                value: query[propertyName],
-            }));
-    }
+  isRelation(propertyName) {
+      return Object.prototype.hasOwnProperty.call(this.getModelRelations(), propertyName);
+  }
 
-    getName() {
-        return this.model.modelName;
-    }
+  getQueriedProperties(query = {}, options={}) {
+      return Object
+          .keys(query)
+          .filter(propertyName => this.isProperty(propertyName))
+          .map(propertyName => ({
+              key: this.getColumnName(propertyName, options),
+              value: query[propertyName],
+          }));
+  }
 
-    as(alias) {
-        return new ModelWrapper(this.model, alias);
-    }
+  getName() {
+      return this.model.modelName;
+  }
 
-    static fromModel(model, alias = null) {
-        return new ModelWrapper(model, alias);
-    }
+  as(alias) {
+      return new ModelWrapper(this.model, alias);
+  }
+
+  static fromModel(model, alias = null) {
+      return new ModelWrapper(model, alias);
+  }
 };
